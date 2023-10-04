bcf14d161c469dd595a302f395444c1b26a91561
Marco Rehmer <mrehmer-services@mailbox.org>
2023-10-04 14:46:35 +0200

    move backend to seperate directory

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,6 +1,6 @@
-import { Logger } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app/app.module';
+import { Logger } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app/app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {cors: true});
---
+++ backend/src/models/users/users.module.ts
@@ -0,0 +1,15 @@
+import { Module } from "@nestjs/common";
+
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserEntity } from "./user.entity";
+import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
+
+@Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
+  controllers: [UsersController],
+  providers: [UsersService],
+  exports: [UsersService]
+})
+export class UsersModule {
+}
