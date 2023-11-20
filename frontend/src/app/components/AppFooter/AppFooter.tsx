a779cf67829a766f1453086bb7124643a79f1cba
Marco Rehmer <mrehmer-services@mailbox.org>
2023-11-20 14:58:19 +0100

    remove sample footer

--- frontend/src/app/components/AppFooter/AppFooter.tsx
+++ frontend/src/app/components/AppFooter/AppFooter.tsx
@@ -3,22 +3,5 @@ import ChangeHistoryIcon from '@mui/icons-material/ChangeHistory';
 import Typography from '@mui/material/Typography';
 
 export const AppFooter = () => {
-  const monthlyHours = '130';
-  const dailyHours = '6:30';
-  const delta = '-3,15h';
-
-  return (
-    <div className={styles.container}>
-      <span>
-        <Typography>Month: {monthlyHours}h</Typography>
-      </span>
-      <span>
-        <Typography>Today: {dailyHours}h</Typography>
-      </span>
-      <span className={styles['delta-container']}>
-        <ChangeHistoryIcon />
-        <Typography>{delta}h</Typography>
-      </span>
-    </div>
-  );
+  return <></>;
 };
