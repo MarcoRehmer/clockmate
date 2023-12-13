3d3a496908afed556c842a985384a15781f126e9
Marco Rehmer <mrehmer-services@mailbox.org>
2023-12-13 06:38:59 +0100

    chore: some renamings and metadata text updates

--- frontend/src/app/(auth)/layout.tsx
+++ frontend/src/app/(auth)/layout.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
-export default function LoginLayout({ children }: { children: React.ReactNode }) {
+export const metadata = {
+    title: 'Clockmate | Login',
+    description: 'Your Time Tracking Buddy',
+};
+
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
       <>
           <div>Welcome to Clockmate</div>
