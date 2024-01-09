4cbc7d954f0b430591bc4a01c81536f8d443227e
Marco Rehmer <mrehmer-services@mailbox.org>
2024-01-09 12:55:31 +0100

    show snackbar, if some different error occours than 401

--- frontend/src/app/(auth)/login/page.tsx
+++ frontend/src/app/(auth)/login/page.tsx
@@ -3,10 +3,9 @@
 import { LoginForm } from '@/app/(auth)/login/LoginForm';
 import { useRouter } from 'next/navigation';
 import { api } from '@/app/api/api';
-import { Box, Card, CircularProgress, Fade } from '@mui/material';
+import { Alert, Box, Card, CircularProgress, Fade, Snackbar } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import React, { useState } from 'react';
-import { sleep } from '@/app/utils/sleep';
 import { isAxiosError } from 'axios';
 
 async function encryptPassword(password: string): Promise<string> {
@@ -21,6 +20,7 @@ export default function Index() {
   const router = useRouter();
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined);
+  const [serverError, setServerError] = useState(false);
 
   const loginRedirect = async (email: string, password: string) => {
     setLoading(true);
@@ -38,10 +38,10 @@ export default function Index() {
         if (e.response?.status === 401) {
           setErrorMessage('invalid username or password');
         } else {
-          // show "something went wrong"
+          setServerError(true);
         }
       } else {
-        // show "something went wrong"
+        setServerError(true);
       }
     } finally {
       setLoading(false);
@@ -49,29 +49,41 @@ export default function Index() {
   };
 
   return (
-    <Box
-      style={{
-        maxWidth: 360,
-        margin: 'auto',
-        display: 'flex',
-        height: '80vh',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Fade in={true} timeout={600}>
-        <Card style={{ padding: 24 }}>
-          <Typography variant="h6" sx={{ color: 'primary.main', textAlign: 'center', mb: 2 }}>
-            Welcome to Clockmate
-          </Typography>
-          <LoginForm loginRequested={loginRedirect} errorMessage={errorMessage} />
-          {loading && (
-            <Box sx={{ display: 'flex', justifyContent: 'center' }}>
-              <CircularProgress />
-            </Box>
-          )}
-        </Card>
-      </Fade>
-    </Box>
+    <>
+      <Box
+        style={{
+          maxWidth: 360,
+          margin: 'auto',
+          display: 'flex',
+          height: '80vh',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Fade in={true} timeout={600}>
+          <Card style={{ padding: 24 }}>
+            <Typography variant="h6" sx={{ color: 'primary.main', textAlign: 'center', mb: 2 }}>
+              Welcome to Clockmate
+            </Typography>
+            <LoginForm loginRequested={loginRedirect} errorMessage={errorMessage} />
+            {loading && (
+              <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+                <CircularProgress />
+              </Box>
+            )}
+          </Card>
+        </Fade>
+      </Box>
+
+      <Snackbar
+        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+        open={serverError}
+        onClose={() => setServerError(false)}
+      >
+        <Alert onClose={() => setServerError(false)} severity="error" sx={{ width: '100%' }}>
+          Something went wrong!
+        </Alert>
+      </Snackbar>
+    </>
   );
 }
