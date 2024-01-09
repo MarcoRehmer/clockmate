adbe0ea2be90f61b57ba5ec7e6dd0e13721c422b
Marco Rehmer <mrehmer-services@mailbox.org>
2024-01-09 12:36:24 +0100

    add formControl error status, if mail or password is not set

--- frontend/src/app/(auth)/login/LoginForm.tsx
+++ frontend/src/app/(auth)/login/LoginForm.tsx
@@ -1,17 +1,47 @@
-import { Button, FormControl, FormGroup, IconButton, Input, InputAdornment, InputLabel } from '@mui/material';
+import {
+  Button,
+  FormControl,
+  FormGroup,
+  FormHelperText,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+} from '@mui/material';
 import React, { useState } from 'react';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
 
-export const LoginForm = ({ loginRequested }: { loginRequested: (email: string, password: string) => void }) => {
+export const LoginForm = ({
+  loginRequested,
+  errorMessage,
+}: {
+  loginRequested: (email: string, password: string) => void;
+  errorMessage: string | undefined;
+}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [showPassword, setShowPassword] = useState(false);
+  const [emptyUsernameError, setEmptyUsernameError] = useState(false);
+  const [emptyPasswordError, setEmptyPasswordError] = useState(false);
+
+  const onLoginSubmitted = () => {
+    const emailEmpty = email.trim().length === 0;
+    const passwordEmpty = password.trim().length === 0;
+
+    setEmptyUsernameError(emailEmpty);
+    setEmptyPasswordError(passwordEmpty);
+
+    if (!emailEmpty && !passwordEmpty) {
+      loginRequested(email, password);
+    }
+  };
 
   return (
     <>
-      <FormGroup onKeyDown={({ key }) => key === 'Enter' && loginRequested(email, password)}>
+      <FormGroup onKeyDown={({ key }) => key === 'Enter' && onLoginSubmitted()}>
+        <FormHelperText error={!errorMessage}>{errorMessage}</FormHelperText>
         <div style={{ display: 'flex', flexDirection: 'column', rowGap: '1rem' }}>
-          <FormControl variant="standard">
+          <FormControl variant="standard" error={emptyUsernameError}>
             <InputLabel htmlFor="email">E-Mail</InputLabel>
             <Input
               id="email"
@@ -20,7 +50,7 @@ export const LoginForm = ({ loginRequested }: { loginRequested: (email: string, 
               value={email}
             />
           </FormControl>
-          <FormControl variant="standard">
+          <FormControl variant="standard" error={emptyPasswordError}>
             <InputLabel htmlFor="password">Password</InputLabel>
             <Input
               id="password"
@@ -41,7 +71,7 @@ export const LoginForm = ({ loginRequested }: { loginRequested: (email: string, 
               }
             ></Input>
           </FormControl>
-          <Button onClick={() => loginRequested(email, password)}>Login</Button>
+          <Button onClick={onLoginSubmitted}>Login</Button>
         </div>
       </FormGroup>
     </>
