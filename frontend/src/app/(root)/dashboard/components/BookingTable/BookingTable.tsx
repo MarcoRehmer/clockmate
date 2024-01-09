355c7bbe9eafc71e6db70803878a3f9d8f4690c5
Marco Rehmer <mrehmer-services@mailbox.org>
2024-01-09 11:10:56 +0100

    add duration to every booking row

--- frontend/src/app/(root)/dashboard/components/BookingTable/BookingTable.tsx
+++ frontend/src/app/(root)/dashboard/components/BookingTable/BookingTable.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { IconButton, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { Box, IconButton, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import { useContext, useEffect, useState } from 'react';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { DeleteBookingDialog } from '../DeleteBookingDialog/DeleteBookingDialog';
@@ -9,6 +9,7 @@ import { Activity } from '@/app/core/types';
 import React from 'react';
 
 import Typography from '@mui/material/Typography';
+import { DateTime } from 'luxon';
 
 interface BookingTableProps {
   activities: Array<Activity>;
@@ -36,14 +37,18 @@ export const BookingTable = (props: BookingTableProps) => {
     }
   };
 
+  const calcDuration = (startedAt: DateTime, finishedAt: DateTime) => {
+    const diff = finishedAt.diff(startedAt);
+    return `${Math.floor(diff.as('hours'))}:${`${Math.floor(diff.as('minutes') % 60)}`.padStart(2, '0')}`;
+  };
+
   return (
     <>
       <TableContainer>
-        <Table style={{ tableLayout: 'auto' }}>
+        <Table style={{ tableLayout: 'auto' }} size="small">
           <TableHead>
             <TableRow>
-              <TableCell>From</TableCell>
-              <TableCell>To</TableCell>
+              <TableCell>Duration</TableCell>
               <TableCell>Remarks</TableCell>
               <TableCell></TableCell>
             </TableRow>
@@ -53,7 +58,7 @@ export const BookingTable = (props: BookingTableProps) => {
               <React.Fragment key={row.id}>
                 {index === 0 || (index > 0 && row.startedAt.day !== props.activities[index - 1].startedAt.day) ? (
                   <TableRow>
-                    <TableCell colSpan={4}>
+                    <TableCell colSpan={4} sx={{ paddingTop: 1.5, paddingBottom: 1.5 }}>
                       <Typography sx={{ color: 'text.primary' }}>
                         {row.startedAt.toFormat('EEEE, dd.MM.yyyy')}
                       </Typography>
@@ -62,24 +67,34 @@ export const BookingTable = (props: BookingTableProps) => {
                 ) : (
                   <></>
                 )}
-                <TableRow>
+                <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  {/* Duration cell */}
                   <TableCell
                     sx={{
                       color: 'text.secondary',
-                      width: 80,
+                      width: 100,
                     }}
                   >
-                    {row.startedAt.toFormat('HH:mm')}
+                    <div>
+                      <Typography fontSize="large" sx={{ textAlign: 'center' }}>
+                        {calcDuration(row.startedAt, row.finishedAt || DateTime.now())}
+                      </Typography>
+                      <Box sx={{ display: 'flex', columnGap: 0.5, flexDirection: 'row' }}>
+                        <span>{row.startedAt.toFormat('HH:mm')}</span>-
+                        <span>{row.finishedAt?.toFormat('HH:mm') || '-'}</span>
+                      </Box>
+                    </div>
                   </TableCell>
-                  <TableCell
-                    sx={{
-                      color: 'text.secondary',
-                      width: 80,
-                    }}
-                  >
-                    {row.finishedAt?.toFormat('HH:mm') || ''}
+
+                  {/* Remarks Cell */}
+                  <TableCell>
+                    <Typography fontSize="medium" sx={{ color: 'text.secondary' }}>
+                      {' '}
+                      {row.remarks}
+                    </Typography>
                   </TableCell>
-                  <TableCell sx={{ color: 'text.secondary' }}>{row.remarks}</TableCell>
+
+                  {/* Conetxt Menu Cell */}
                   <TableCell sx={{ padding: 0, width: '1rem' }}>
                     <IconButton
                       aria-label="menu"
